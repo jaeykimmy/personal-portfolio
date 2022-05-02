4d4b233a74c5161ec4ccb5c565e8e53cc79990fb
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-05-02 15:03:19 -0400

    inital push

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Topbar from "./components/Topbar/Topbar";
+import Intro from "./components/Intro/Intro";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Works from "./components/Works/Works";
+import Contact from "./components/Contact/Contact"
+import "./App.scss"
+import { useState } from "react";
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(true)
+  return (
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <div className="sections">
+      <Intro />
+      <Portfolio />
+      <Works />
+      <Testimonials />
+      <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
