aafbb74684166fb871ac43eedadaf94d4656e96f
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-05-03 20:43:50 -0400

    intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import "./Intro.scss"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
 export default function Intro() {
   return (
-    <div className='intro' id="intro"></div>
+    <div className='intro' id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.jpg" alt=""/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Jaeyoung Kim</h1>
+          <h3>Full Stack<span></span></h3>
+        </div>
+        <a href="portfolio">
+          <KeyboardArrowDownIcon className={'arrow'}/>
+        </a>
+      </div>
+    </div>
   )
 }
