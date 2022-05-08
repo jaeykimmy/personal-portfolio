05b8283b27d9feffd6fe910eb4cd0cea7fb889e6
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-05-08 12:17:38 -0400

    portfolio with dummydata and useeffect

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import PortfolioList from '../PortfolioList/PortfolioList'
 import "./Portfolio.scss"
+import { useState } from 'react'
+import { featuredPortfolio, reactPortfolio, webPortfolio } from '../../data'
+
 export default function Portfolio() {
+  const [selected, setSelected] = useState('featured')
+  const [data, setData] = useState([])
+  const list = [
+    {
+      id: "featured",
+      title: "Featured"
+    },
+    {
+      id: "react",
+      title: "React"
+    },
+    {
+      id: "web",
+      title: "Web"
+    },
+  ]
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "react":
+        setData(reactPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  },[selected])
   return (
-    <div className="portfolio" id="portfolio"></div>
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map(item => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id} />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((data) => (
+          
+          <div className="item">
+            <img src={data.img} alt=""/>
+          <h3>{data.title}</h3>
+        </div>
+        ))}
+      </div>
+    </div>
   )
 }
---
+++ src/components/PortfolioList/PortfolioList.jsx
@@ -0,0 +1,13 @@
+import "./PortfolioList.scss"
+
+import React from 'react'
+
+export default function PortfolioList({id, title, active, setSelected}) {
+  return (
+    <li
+      className={active ? "portfolioList active" : "portfolioList"}
+      onClick={() => setSelected(id)}>
+      {title}
+    </li>
+  )
+}
