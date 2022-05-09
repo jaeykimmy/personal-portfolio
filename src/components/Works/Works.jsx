6068df2cb902e115c79273edde14655a9fed5f98
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-05-08 20:48:26 -0400

    add react project to slider

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -5,8 +5,25 @@ export default function Works() {
   return (
     <div className="works" id="works">
       <div className="slider">
-        
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" alt="" />
+                </div>
+                <h2>React</h2>
+                <p>I completed a pair programming end-to-end React project using the MERN stack and created a React app based on my own passion - Pokemon</p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img src="https://raw.githubusercontent.com/rosemaryku/mentor-me/main/docs/search.gif" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
+      <img src="" alt="" />
     </div>
   )
 }
