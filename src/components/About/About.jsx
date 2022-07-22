2cba39a7dfd0bad67465745ce80ff0fd68618f7b
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-07-22 17:51:17 -0400

    remove distortion on mobile for background on icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import "./About.scss";
 export default function About() {
   return (
-    <div className="about" id="about">
-      <h1>About Me</h1>
-      <p>
-        I am a full stack web developer transitioning from an experienced
-        healthcare worker role as an x-ray technologist. Due to my knack for
-        picking up the electronic health record systems quickly and being
-        entrusted by the senior technologists to deal with many software issues
-        and bugs, I decided to leave my full-time role to work a career doing
-        what I love - solving problems and making things.
-      </p>
-      <p>
-        I have since worked to become proficient using React, Express,
-        PostgreSQL and Node.js. I have experience in a pair programming setting
-        as well as using test driven development to ensure my code stays
-        performant and efficient. My expertise lies in front-end development
-        using MUI, Tailwind CSS, and SCSS to style my work based on industry
-        standards.
-      </p>
-    </div>
+    <>
+      <div className="about" id="about">
+        <h1>About Me</h1>
+        <p>
+          I am a full stack web developer transitioning from an experienced
+          healthcare worker role as an x-ray technologist. Due to my knack for
+          picking up the electronic health record systems quickly and being
+          entrusted by the senior technologists to deal with many software
+          issues and bugs, I decided to leave my full-time role to work a career
+          doing what I love - solving problems and making things.
+        </p>
+        <p>
+          I have since worked to become proficient using React, Express,
+          PostgreSQL and Node.js. I have experience in a pair programming
+          setting as well as using test driven development to ensure my code
+          stays performant and efficient. My expertise lies in front-end
+          development using MUI, Tailwind CSS, and SCSS to style my work based
+          on industry standards.
+        </p>
+      </div>
+    </>
   );
 }
