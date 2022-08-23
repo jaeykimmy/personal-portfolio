313ce1abe0563f655c3b08c925a55f8130fdbd67
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-08-23 15:34:16 -0400

    new logo for initial

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            Jaeyoung
+            J
           </a>
         </div>
         <div className="right">
